be0645d7473e5348069052a9d83ec8239a7474d0
Shaharyar Aziz <shaharyaraziz462@gmail.com>
2025-08-28 14:26:22 +0500

    Login and SignUp

---
+++ URL/url.ts
@@ -0,0 +1,2 @@
+let url="https://beyond-styles-backend-production-8c7f.up.railway.app/api"
+export {url};
---
+++ tests/pages/userApi.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+import { url } from '../../URL/url';
+
+test.describe('User API Testing', () => {
+  test('Signup Users', async ({ request }) => {
+    const randomNum = Math.floor(Math.random() * 1000);
+    const payload = {
+      first_name: `Test${randomNum}`,
+      last_name: "ahmad",
+      email: `testuser${randomNum}@gmail.com`,
+      phone: `+9230012345${randomNum}`,
+      user_type: "client",
+      password: "test1234",
+      device_token: "device_token"
+    }
+    if (payload.email === "") {
+      console.log("Email is required");
+
+    }
+    const response = await request.post(`${url}/accounts/signup`, {
+      data: payload,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+
+    });
+    expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    console.log("📌 Login API Response:", responseBody);
+  })
+  test('Login Users', async ({ request }) => {
+    const payload = {
+      email: "testuser13@gmail.com",
+      password: "test1234"
+    }
+    const response = await request.post(`${url}/accounts/login`, {
+      data: payload,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    console.log("📌 Login API Response:", responseBody);
+
+  })
+})
